5a168391add7e707710b7b8aa6db3454d01aaa85
adamwarniment-infotrust <52217286+adamwarniment-infotrust@users.noreply.github.com>
2021-04-26 10:39:24 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 console.log('js loaded');
 // Test setting a SameParty cookie with FPS
-document.cookie = "fpsCookie=testValue; SameSite=Lax; Secure; SameParty";
+document.cookie = "cookieNeSample=tasty; SameSite=Lax; Secure; SameParty; expires=Fri, 31 Dec 9999 23:59:59 GMT";
